7b1173f2bebcf983d178d252a6eb68199236cbe8
ronpandolfi <ronpandolfi@gmail.com>
2017-10-19 19:45:30 -0700

    Moving plugins to separate repo

---
+++ IProcessingPlugin.py
@@ -0,0 +1,17 @@
+from yapsy.IPlugin import IPlugin
+
+
+class IProcessingPlugin(IPlugin):
+    def __init__(self, *args, **kwargs):
+        super(IProcessingPlugin, self).__init__()
+
+    def evaluate(self):
+        raise NotImplementedError
+
+
+class Input(object):
+    def __init__(self, name='', description='', default=None, unit=None, min=None, max=None, bounds=None):
+        self.name = name
+        self.description = description
+        self.default = default
+        self.unit = unit
