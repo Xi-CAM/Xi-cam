8423c83d819b7b238c45413f18189de003995dda
Ron Pandolfi <ronpandolfi@gmail.com>
2017-10-27 13:07:11 -0700

    Add MainWindow testing.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+from qtpy.QtCore import *
 from qtpy.QtWidgets import *
 
 
@@ -14,5 +15,18 @@ def mainloop():
 def test_splash():
     from ..windows import splash
     app = makeapp()
-    splash = splash.XicamSplashScreen()
+    splash = splash.XicamSplashScreen(QMainWindow)
+    mainloop()
+
+
+def test_mainwindow():
+    from ..windows.mainwindow import XicamMainWindow
+
+    app = makeapp()
+    window = XicamMainWindow()
+    window.show()
+
+    t = QTimer()
+    # t.singleShot(1000, window.close)
+
     mainloop()
