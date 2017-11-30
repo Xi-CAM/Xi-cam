3a7b449bede563ce3db66a819fc511f6ddb215b5
Ron Pandolfi <ronpandolfi@gmail.com>
2017-11-30 15:31:16 -0800

    Move dir structure for namespace package support

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages('xicam/gui', exclude=['contrib', 'docs', 'tests']),
 
     package_dir={},
 
--- static/__init__.py
+++
@@ -1,5 +0,0 @@
-import pathlib
-
-
-def path(item):
-    return pathlib.Path(pathlib.Path(__file__).parent, item)
