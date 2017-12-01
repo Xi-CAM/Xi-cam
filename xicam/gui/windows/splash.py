d7eab72f619e1bc0d1065e3f1524b9e1fb70737a
Ron Pandolfi <ronpandolfi@gmail.com>
2017-11-30 19:41:58 -0800

    Add images to setup.py with manifest.in

--- xicam/gui/windows/splash.py
+++ xicam/gui/windows/splash.py
@@ -84,7 +84,7 @@ class XicamSplashScreen(QSplashScreen):
         if self._launchready:
             self.execlaunch()
             return
-        self.timer.singleShot(0.000001, self.movie.start)
+        self.movie.start()
 
     def launchwindow(self):
         """
