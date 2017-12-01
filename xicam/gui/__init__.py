58f1fcf7029f906744f0a1fac2a94c593054dc1a
Ron Pandolfi <ronpandolfi@gmail.com>
2017-11-30 19:04:12 -0800

    Add explicit imports to enforce loading core modules before activating venv

---
+++ xicam/gui/__init__.py
@@ -0,0 +1,7 @@
+# Explicitly import things so that they are loaded before activating a venv
+from . import cammart
+from . import settings
+from . import static
+from . import threads
+from . import widgets
+from . import windows
