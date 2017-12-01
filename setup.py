e1932e8da29bcc389ca0f7c75adf26460183b1f1
Ron Pandolfi <ronpandolfi@gmail.com>
2017-11-30 18:52:04 -0800

    Add appdirs to requirements.

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['yapsy', 'astropy', 'signalslot', 'virtualenv', 'requests'],
+    install_requires=['yapsy', 'astropy', 'signalslot', 'virtualenv', 'requests', 'appdirs'],
 
     setup_requires=[],
 
