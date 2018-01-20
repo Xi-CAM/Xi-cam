b5140ffd025c43cc56e4877a3986ff5828d135e8
Ron Pandolfi <ronpandolfi@gmail.com>
2018-01-19 16:23:05 -0800

    Add tests

---
+++ xicam/gui/tests/test_threads.py
@@ -0,0 +1,22 @@
+def test_threads():
+    from xicam.gui import threads
+    from qtpy.QtWidgets import QApplication
+    from qtpy.QtCore import QTimer, QObject, Signal
+    app = QApplication([])
+    q = QTimer()
+
+    def callback(a):
+        assert a == 10
+
+    t = threads.QThreadFuture(sum, [1, 2, 3, 4], callback_slot=callback)
+
+    class Callback(QObject):
+        sig = Signal(int)
+
+    callback = Callback()
+    t2 = threads.QThreadFuture(sum, [1, 2, 3, 4], callback_slot=callback.sig)
+
+    q.singleShot(1000, t.start)
+    q.singleShot(1000, t2.start)
+    q.singleShot(2000, app.quit)
+    app.exec_()
