7a923f611f8d13e7fbfd7233421098548d1c5dc3
Ron Pandolfi <ronpandolfi@gmail.com>
2018-01-28 18:11:09 -0800

    test improvements

--- xicam/core/tests/test_msg.py
+++ xicam/core/tests/test_msg.py
@@ -1,10 +1,10 @@
 def test_msg():
-    from .. import msg
+    from xicam.core import msg
     msg.logMessage('this', 'is', 'a', 'tests:', 42, level=msg.WARNING)
 
 
 def test_exception_msg():
-    from .. import msg  # force load the message module
+    from xicam.core import msg  # force load the message module
     import pytest
 
     # Raise an error; this should be
