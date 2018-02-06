ed0d6c1f88ad971dfcc1a3ac06577a901091bbd7
Ron Pandolfi <ronpandolfi@gmail.com>
2018-02-06 12:26:24 -0800

    Add workflow observer pattern and process manipulation

--- xicam/core/execution/workflow.py
+++ xicam/core/execution/workflow.py
@@ -1,10 +1,12 @@
 from xicam.plugins import ProcessingPlugin
+from typing import Callable
 
 # TODO: add debug flag that checks mutations by hashing inputs
 
 class Workflow(object):
     def __init__(self, processes=None):
         self._processes = []
+        self._observers = []
         if processes:
             self._processes.extend(processes)
         self.staged = False
@@ -20,7 +22,28 @@ class Workflow(object):
             If True, connects Outputs of the previously added Process to the Inputs of process, matching names and types
         """
         self._processes.append(process)
+        self.update()
         # TODO: Add autoconnect functionality
+
+    def insertProcess(self, index: int, process: ProcessingPlugin, autoconnect: bool = False):
+        self._processes.insert(index, process)
+        self.update()
+
+    def removeProcess(self, process):
+        self._processes.remove(process)
+        self.update()
+
+    def disableProcess(self, process):
+        pass  # TODO: allow processes to be disabled
+
+    @property
+    def processes(self):
+        return self._processes
+
+    @processes.setter
+    def processes(self, processes):
+        self._processes = processes
+        self.update()
 
     def connect(self, input, output):
         # Connect any two of the following: Input, Output, Signal, Slot
@@ -72,3 +95,13 @@ class Workflow(object):
         """
         # TODO: add validation
         return True
+
+    def attach(self, observer: Callable):
+        self._observers.append(observer)
+
+    def detatch(self, observer: Callable):
+        self._observers.remove(observer)
+
+    def update(self):
+        for observer in self._observers:
+            observer()
