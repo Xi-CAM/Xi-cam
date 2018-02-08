6114f353789dea16eca5ed99d40f80b40094d7b5
Ron Pandolfi <ronpandolfi@gmail.com>
2018-02-08 09:47:23 -0800

    Soft load default widgets

--- xicam/gui/widgets/__init__.py
+++ xicam/gui/widgets/__init__.py
@@ -3,6 +3,9 @@ from .motd import MOTD
 from .dataresourcebrowser import DataResourceBrowser
 from .previewwidget import PreviewWidget
 
-defaultstage = GUILayout(center=MOTD(),
+from qtpy.QtWidgets import QApplication
+
+if QApplication.instance():
+    defaultstage = GUILayout(center=MOTD(),
                          left=DataResourceBrowser(),
                          lefttop=PreviewWidget())
