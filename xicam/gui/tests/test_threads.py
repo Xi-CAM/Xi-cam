8efbd1ba78eaf4fb094d049ca13b6f39bbd65ee4
ronpandolfi <ronpandolfi@gmail.com>
2018-02-18 08:59:54 -0800

    add qthreadfutureiterator test

--- xicam/gui/tests/test_threads.py
+++ xicam/gui/tests/test_threads.py
@@ -20,3 +20,30 @@ def test_threads():
     q.singleShot(1000, t2.start)
     q.singleShot(2000, app.quit)
     app.exec_()
+
+
+def test_threads_iterator():
+    from qtpy.QtWidgets import QApplication
+    app = QApplication([])
+    from xicam.gui import threads
+    from qtpy.QtCore import QTimer, QObject, Signal
+    q = QTimer()
+
+    results = []
+
+    def callback(a):
+        results.append(a)
+
+    def testiterator():
+        for i in range(3):
+            yield i
+
+    def check():
+        assert sum(results) == 3
+
+    t = threads.QThreadFutureIterator(testiterator, callback_slot=callback, finished_slot=check)
+
+    q.singleShot(1000, t.start)
+    q.singleShot(2000, app.quit)
+
+    app.exec_()
