b2a035a4c5731b5f72d4a440cb21783cdf3b39b7
ronpandolfi <ronpandolfi@gmail.com>
2018-03-15 11:10:32 -0700

    Add imageviewer parameter

--- xicam/gui/patches/PyQtGraph.py
+++ xicam/gui/patches/PyQtGraph.py
@@ -3,6 +3,7 @@ This patch module adds color gradients like matplotlib's viridis etc. to PyQtGra
 '''
 from pyqtgraph.graphicsItems import GradientEditorItem
 from collections import OrderedDict
+from qtpy.QtWidgets import QTreeWidgetItem
 
 GradientEditorItem.__dict__['Gradients'] = OrderedDict([
     ('thermal', {'ticks': [(0.3333, (185, 0, 0, 255)), (0.6666, (255, 220, 0, 255)), (1, (255, 255, 255, 255)), (0, (0, 0, 0, 255))], 'mode': 'rgb'}),
@@ -18,3 +19,72 @@ GradientEditorItem.__dict__['Gradients'] = OrderedDict([
     ('greyclip', {'ticks': [(0.0, (0, 0, 0, 255)), (0.99, (255, 255, 255, 255)), (1.0, (255, 0, 0, 255))], 'mode': 'rgb'}),
     ('grey', {'ticks': [(0.0, (0, 0, 0, 255)), (1.0, (255, 255, 255, 255))], 'mode': 'rgb'}),
 ])
+
+from pyqtgraph.parametertree import Parameter, ParameterItem, registerParameterType
+from pyqtgraph.parametertree.parameterTypes import WidgetParameterItem
+from pyqtgraph import ImageView
+import numpy as np
+
+
+class ImageParameterItem(WidgetParameterItem):
+    def makeWidget(self):
+        self.subItem = QTreeWidgetItem()
+        self.addChild(self.subItem)
+
+        w = ImageView()
+        w.value = lambda: w.image
+        w.setValue = w.setImage
+        w.sigChanged = None
+
+        self.hideWidget = False
+        return w
+
+    def treeWidgetChanged(self):
+        ## TODO: fix so that superclass method can be called
+        ## (WidgetParameter should just natively support this style)
+        # WidgetParameterItem.treeWidgetChanged(self)
+        self.treeWidget().setFirstItemColumnSpanned(self.subItem, True)
+        self.treeWidget().setItemWidget(self.subItem, 0, self.widget)
+
+        # for now, these are copied from ParameterItem.treeWidgetChanged
+        self.setHidden(not self.param.opts.get('visible', True))
+        self.setExpanded(self.param.opts.get('expanded', True))
+
+    def valueChanged(self, param, val, force=False):
+        ## called when the parameter's value has changed
+        ParameterItem.valueChanged(self, param, val)
+        if force or not np.array_equal(val, self.widget.value()):
+            self.widget.setValue(val.astype(np.float))
+        self.updateDisplayLabel(val)  ## always make sure label is updated, even if values match!
+
+    def updateDefaultBtn(self):
+        pass
+
+
+class ImageParameter(Parameter):
+    itemClass = ImageParameterItem
+
+    def __init__(self, *args, **kwargs):
+        if 'expanded' not in kwargs: kwargs['expanded'] = False
+        super(ImageParameter, self).__init__(*args, **kwargs)
+
+    def setValue(self, value, blockSignal=None):
+        """
+        Set the value of this Parameter; return the actual value that was set.
+        (this may be different from the value that was requested)
+        """
+        try:
+            if blockSignal is not None:
+                self.sigValueChanged.disconnect(blockSignal)
+            if np.array_equal(self.opts['value'], value):
+                return value
+            self.opts['value'] = value
+            # self.sigValueChanged.emit(self, value)
+        finally:
+            if blockSignal is not None:
+                self.sigValueChanged.connect(blockSignal)
+
+        return value
+
+
+registerParameterType('ndarray', ImageParameter, override=True)
