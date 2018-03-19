5d42f058b758502636bde02d71e6511903ce778e
ronpandolfi <ronpandolfi@gmail.com>
2018-03-19 15:41:49 -0700

    Remove unused import

--- xicam/core/execution/localexecutor.py
+++ xicam/core/execution/localexecutor.py
@@ -1,6 +1,7 @@
 from dask.diagnostics import Profiler, ResourceProfiler, CacheProfiler
 from dask.diagnostics import visualize
 import dask.threaded
+from distributed import LocalCluster
 from xicam.core import msg
 from appdirs import user_config_dir
 from .daskexecutor import DaskExecutor
