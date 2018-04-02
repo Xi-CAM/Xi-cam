54cdb46f74689113f92d5296b733ae5427ca3013
ronpandolfi <ronpandolfi@gmail.com>
2018-04-02 13:32:49 -0700

    Hide timeline when t axis has len 1

--- xicam/gui/widgets/dynimageview.py
+++ xicam/gui/widgets/dynimageview.py
@@ -12,6 +12,9 @@ class DynImageView(ImageView):
         # Shrink LUT
         self.getHistogramWidget().setMinimumWidth(10)
 
+        # Don't invert Y axis
+        self.view.invertY(False)
+
     def quickMinMax(self, data):
         """
         Estimate the min/max values of *data* by subsampling. MODIFIED TO USE THE 99TH PERCENTILE instead of max.
@@ -19,3 +22,10 @@ class DynImageView(ImageView):
         if data.size > 1e6:
             data = data[len(data) // 2]
         return np.nanmin(data), np.nanpercentile(data, 99)
+
+    def setImage(self, img, autoRange=True, autoLevels=True, levels=None, axes=None, xvals=None, pos=None, scale=None,
+                 transform=None, autoHistogramRange=True):
+        super(DynImageView, self).setImage(img, autoRange, autoLevels, levels, axes, xvals, pos, scale, transform,
+                                           autoHistogramRange)
+        if len(img.shape) > 2 and img.shape[0] == 1:
+            self.ui.roiPlot.hide()
