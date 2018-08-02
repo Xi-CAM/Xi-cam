0387bb5dd454fcd3dbe3ee58566ff65a3cbe3e86
ronpandolfi <ronpandolfi@gmail.com>
2018-08-02 13:28:31 -0700

    Fix typo

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 import os
-if 'APPVEYOR' in os.environ and os.environ['APPVERYOR']:
+if 'APPVEYOR' in os.environ and os.environ['APPVEYOR']:
     pyqt=['PyQt5']
 else:
     pyqt=[]
