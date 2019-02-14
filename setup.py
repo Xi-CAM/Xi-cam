a5b931d208991dddb388400eb1ea7665dbbe2f2d
Thomas A Caswell <tcaswell@bnl.gov>
2019-02-14 16:51:43 -0500

    MNT: pathlib is in standard library for py3

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['databroker', 'pathlib', 'distributed', 'dask'],
+    install_requires=['databroker', 'distributed', 'dask'],
 
     setup_requires=[],
 
