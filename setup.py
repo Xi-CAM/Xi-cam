07f0b9b1a395f8431e3e079b2fc7c1fba69a25a1
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-02-15 15:47:09 -0800

    Remove deprecated dependency

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['yapsy', 'astropy', 'signalslot', 'virtualenv', 'requests', 'appdirs', 'fabio'], # TODO: deprecate fabio
+    install_requires=['yapsy', 'astropy', 'signalslot', 'virtualenv', 'requests', 'appdirs'],
 
     setup_requires=[],
 
