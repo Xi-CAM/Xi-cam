5f05684073ebed8497a1c5c90ac3930e43a9d253
Dan Allan <dallan@bnl.gov>
2019-04-23 12:38:33 -0400

    WIP: BaselineWidget works.

--- bluesky_browser/viewer.py
+++ bluesky_browser/viewer.py
@@ -1,5 +1,8 @@
+from event_model import RunRouter, unpack_event_page
+from qtpy.QtGui import QStandardItemModel, QStandardItem
 from qtpy.QtWidgets import (
     QWidget,
+    QTableView,
     QTabWidget,
     )
 
@@ -13,11 +16,14 @@ class Container(QTabWidget):
 
     def show_entries(self, entries):
         for entry in entries:
-            uid = entry.metadata['start']['uid']
+            run_catalog = entry()
+            uid = run_catalog.metadata['start']['uid']
             if uid not in self._runs:
                 # Add new Viewer tab.
-                new_tab = Viewer(entry)
-                self.addTab(new_tab, uid[:8])
+                viewer = Viewer()
+                for name, doc in run_catalog.read_canonical():
+                    viewer.run_router(name, doc)
+                self.addTab(viewer, uid[:8])
                 self._runs.append(uid)
         # Show the last entry in the list.
         index = self._runs.index(uid)
@@ -28,6 +34,36 @@ class Container(QTabWidget):
         self.removeTab(index)
 
 
-class Viewer(QWidget):
-    def __init__(self, entry, *args, **kwargs):
+class Viewer(QTabWidget):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.run_router = RunRouter([self.factory])
+
+    def factory(self, name, start_doc):
+        def subfactory(name, descriptor_doc):
+            if descriptor_doc.get('name') == 'baseline':
+                baseline_widget = BaselineWidget()
+                baseline_model = BaselineModel()
+                baseline_widget.setModel(baseline_model)
+                self.addTab(baseline_widget, 'Baseline')
+                return [baseline_model]
+            else:
+                return []
+        return [], [subfactory]
+
+
+class BaselineModel(QStandardItemModel):
+    def __call__(self, name, doc):
+        if name == 'event_page':
+            print('page', doc)
+            for event in unpack_event_page(doc):
+                self.__call__('event', event)
+        elif name == 'event':
+            print(doc['data'])
+            column = [QStandardItem(str(val)) for val in doc['data'].values()]
+            self.setVerticalHeaderLabels(doc['data'].keys())
+            self.appendColumn(column)
+
+
+class BaselineWidget(QTableView):
+    ...
