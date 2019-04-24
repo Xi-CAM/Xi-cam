2b4e66a59a29759e67f43349f3dbb21900757a8d
Dan Allan <dallan@bnl.gov>
2019-04-24 13:59:48 -0400

    Forgot to add file to commit

---
+++ bluesky_browser/baseline.py
@@ -0,0 +1,40 @@
+from event_model import unpack_event_page
+from qtpy.QtGui import QStandardItemModel, QStandardItem
+from qtpy.QtWidgets import QTableView
+
+
+class BaselineModel(QStandardItemModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setHorizontalHeaderLabels(['Before', 'After'])
+
+    def __call__(self, name, doc):
+        if name == 'event_page':
+            for event in unpack_event_page(doc):
+                self.__call__('event', event)
+        elif name == 'event':
+            column = doc['seq_num'] - 1
+            for row, val in enumerate(val for _, val in sorted(doc['data'].items())):
+                self.setItem(row, column, QStandardItem(str(val)))
+            self.setVerticalHeaderLabels(doc['data'].keys())
+
+
+class BaselineWidget(QTableView):
+    ...
+
+
+class BaselineFactory:
+    def __init__(self, add_tab):
+        self.add_tab = add_tab
+
+    def __call__(self, name, start_doc):
+        def subfactory(name, descriptor_doc):
+            if descriptor_doc.get('name') == 'baseline':
+                baseline_widget = BaselineWidget()
+                baseline_model = BaselineModel()
+                baseline_widget.setModel(baseline_model)
+                self.add_tab(baseline_widget, 'Baseline')
+                return [baseline_model]
+            else:
+                return []
+        return [], [subfactory]
