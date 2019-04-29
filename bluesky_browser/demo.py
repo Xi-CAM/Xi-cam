253db48bcd2ca31e8f34c3206456e71af29c16f1
Dan Allan <dallan@bnl.gov>
2019-04-29 11:37:53 -0400

    Add ND scan examples.

--- bluesky_browser/demo.py
+++ bluesky_browser/demo.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 from suitcase.jsonl import Serializer
 from bluesky import RunEngine
-from ophyd.sim import det, noisy_det, motor, motor1, motor2
-from bluesky.plans import scan, count
+from ophyd.sim import det, det4, noisy_det, motor, motor1, motor2, img
+from bluesky.plans import scan, count, grid_scan
 from bluesky.preprocessors import SupplementalData
 from event_model import RunRouter
 import intake_bluesky.jsonl  # noqa; to force intake registration
@@ -11,6 +11,7 @@ import intake_bluesky.jsonl  # noqa; to force intake registration
 
 det.kind = 'hinted'
 noisy_det.kind = 'hinted'
+det4.kind = 'hinted'
 
 
 def generate_example_data(data_path):
@@ -30,7 +31,10 @@ def generate_example_data(data_path):
     RE(count([det]))
     RE(count([noisy_det], 5))
     RE(scan([det], motor, -1, 1, 7))
+    RE(grid_scan([det4], motor1, -1, 1, 4, motor2, -1, 1, 7, False))
+    RE(scan([det], motor, -1, 1, motor2, -1, 1, 5))
     RE(count([noisy_det, det], 5))
+    # RE(count([img], 5))
 
     def factory(name, doc):
         serializer = Serializer(data_path / 'xyz')
