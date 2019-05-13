04b6110a8df9269aa07af5e963f03384f340b840
Dan Allan <dallan@bnl.gov>
2019-05-13 13:29:15 -0400

    FIX: Make Process targets top-level functions.
    This makes them safer to pickle.

--- bluesky_browser/demo.py
+++ bluesky_browser/demo.py
@@ -82,18 +82,56 @@ sources:
     return str(catalog_filepath)
 
 
+def run_proxy(queue):
+    """
+    Run Proxy on random, free ports and communicate the port numbers back.
+    """
+    from bluesky.callbacks.zmq import Proxy
+    proxy = Proxy()
+    queue.put((proxy.in_port, proxy.out_port))
+    proxy.start()
+
+
+def run_publisher(in_port, data_path):
+    """
+    Acquire data in an infinite loop and publish it.
+    """
+    import asyncio
+    from bluesky.callbacks.zmq import Publisher
+    from suitcase.jsonl import Serializer
+    from ophyd.sim import noisy_det, motor1, motor2
+    from bluesky.plans import count
+    from bluesky.preprocessors import SupplementalData
+    from bluesky.plan_stubs import sleep
+    publisher = Publisher(f'localhost:{in_port}')
+    RE = RunEngine(loop=asyncio.new_event_loop())
+    sd = SupplementalData()
+    RE.preprocessors.append(sd)
+    sd.baseline.extend([motor1, motor2])
+    RE.subscribe(publisher)
+
+    def factory(name, doc):
+        serializer = Serializer(data_path / 'abc')
+        serializer('start', doc)
+        return [serializer], []
+
+    rr = RunRouter([factory])
+    RE.subscribe(rr)
+
+    def infinite_plan():
+        while True:
+            yield from sleep(3)
+            yield from count([noisy_det], 20, delay=0.5)
+
+    try:
+        RE(infinite_plan())
+    finally:
+        RE.halt()
+
+
 def stream_example_data(data_path):
     data_path = Path(data_path)
     log.debug(f"Serializing example data into directory {data_path!s}")
-
-    def run_proxy(queue):
-        """
-        Run Proxy on random, free ports and communicate the port numbers back.
-        """
-        from bluesky.callbacks.zmq import Proxy
-        proxy = Proxy()
-        queue.put((proxy.in_port, proxy.out_port))
-        proxy.start()
 
     queue = Queue()
     proxy_process = Process(target=run_proxy, args=(queue,))
@@ -101,43 +139,7 @@ def stream_example_data(data_path):
     in_port, out_port = queue.get()
     log.debug(f"Demo Proxy is listening on port {in_port} and publishing to {out_port}.")
 
-    def run_publisher():
-        """
-        Acquire data in an infinite loop and publish it.
-        """
-        import asyncio
-        from bluesky.callbacks.zmq import Publisher
-        from suitcase.jsonl import Serializer
-        from ophyd.sim import noisy_det, motor1, motor2
-        from bluesky.plans import count
-        from bluesky.preprocessors import SupplementalData
-        from bluesky.plan_stubs import sleep
-        publisher = Publisher(f'localhost:{in_port}')
-        RE = RunEngine(loop=asyncio.new_event_loop())
-        sd = SupplementalData()
-        RE.preprocessors.append(sd)
-        sd.baseline.extend([motor1, motor2])
-        RE.subscribe(publisher)
-
-        def factory(name, doc):
-            serializer = Serializer(data_path / 'abc')
-            serializer('start', doc)
-            return [serializer], []
-
-        rr = RunRouter([factory])
-        RE.subscribe(rr)
-
-        def infinite_plan():
-            while True:
-                yield from sleep(3)
-                yield from count([noisy_det], 20, delay=0.5)
-
-        try:
-            RE(infinite_plan())
-        finally:
-            RE.halt()
-
-    publisher_process = Process(target=run_publisher, args=())
+    publisher_process = Process(target=run_publisher, args=(in_port, data_path))
     publisher_process.start()
     log.debug("Demo acquisition has started.")
 
