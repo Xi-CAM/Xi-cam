21cbd3c327556e3d4ae074400ddd3f7f58b6b5db
Ian Humphrey <ian.r.humphrey@gmail.com>
2019-06-10 09:12:57 -0700

    Clean up sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,6 @@
 from recommonmark.transform import AutoStructify
 from sphinx.ext import autodoc
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'Xi-cam'
@@ -26,16 +25,15 @@ author = 'Ronald Pandolfi'
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   'numpydoc',
-   'recommonmark',
-   'sphinx.ext.autodoc',
+    'numpydoc',
+    'recommonmark',
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,7 +43,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -61,38 +58,28 @@ html_static_path = ['_static']
 
 
 class SimpleClassDocumenter(autodoc.ClassDocumenter):
-   objtype = "simpleclass"
-   content_indent = ""
-   
-   def add_directive_header(self, sig):
-      pass
+    objtype = "simpleclass"
+    # Remove tabs from the docstring rendering
+    content_indent = ""
+
+    def add_directive_header(self, sig):
+        pass
 
 
 class SimpleFunctionDocumenter(autodoc.FunctionDocumenter):
-   objtype = "simplefunction"
-   content_indent = ""
-   
-   def add_directive_header(self, sig):
-      pass
+    objtype = "simplefunction"
+    content_indent = ""
 
+    def add_directive_header(self, sig):
+        pass
 
-def process_signature(app, what, name, obj, options, signature, return_annotation):
-   print("RET: {}", return_annotation)
-   print("app: {}\nwhat: {}\nname: {}\nobj: {}\noptions: {}\nsignature: {}",
-      app, what, name, obj, options, signature)
-
-def process_docstring(app, what, name, obj, options, lines):
-   for line in lines:
-      print("LINE: {}", line)
 
 def setup(app):
+    # Allow md files to evaluate marked sections as rst
     app.add_config_value('recommonmark_config', {
         'enable_eval_rst': True
-        }, True)
+    }, True)
     app.add_transform(AutoStructify)
-   #  app.connect('autodoc-process-signature', process_signature)
-   #  app.connect('autodoc-process-docstring', process_docstring)
-   #  from sphinx.ext.autodoc import cut_lines
-   #  app.connect('autodoc-process-docstring', cut_lines(4, what=['class']))
+
     app.add_autodocumenter(SimpleClassDocumenter)
     app.add_autodocumenter(SimpleFunctionDocumenter)
