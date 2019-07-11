4443f4d8469631dd67b2dae7912a0527890af4d4
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-07-11 13:36:19 -0700

    Travis fixes (#37)
    * Add coverage deps to extras
    
    * Use pip to install extras

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     # $ pip install -e .[dev,tests]
     extras_require={
         'docs': ['sphinx', 'recommonmark', 'sphinx_bootstrap_theme'],
-        'tests': ['pytest', 'coverage'],
+        'tests': ['pytest', 'coverage', 'coveralls', 'codecov'],
         'pyqt5': ['pyqt5'],
         'PySide2': ['PySide2'],
     },
