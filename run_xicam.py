80f9e1bb860a54ba5ba022ccf6d0cf527b5db51c
ronpandolfi <ronpandolfi@gmail.com>
2019-08-01 12:53:32 -0700

    Changes to the splash screen producedure to work under frozen app (also more efficient)

--- run_xicam.py
+++ run_xicam.py
@@ -40,7 +40,17 @@ def main():
     if '-v' in sys.argv:
         QErrorMessage.qtHandler()
 
+    from xicam.gui.windows.mainwindow import XicamMainWindow
     splash = splash.XicamSplashScreen()
+
+    def start():
+        mainwindow = XicamMainWindow()
+        splash.mainwindow = mainwindow
+        mainwindow.load()
+
+    # Start loading the mainwindow within Qt mainloop
+    QTimer.singleShot(0, start)
+
     app.exec_()
 
 
