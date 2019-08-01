44a43e7c06b594b218f1a5f246176a052477e20a
ronpandolfi <ronpandolfi@gmail.com>
2019-08-01 13:03:37 -0700

    Fix pathing when frozen

--- xicam/gui/static/__init__.py
+++ xicam/gui/static/__init__.py
@@ -3,7 +3,4 @@ import sys
 
 
 def path(item: str):
-    if getattr(sys,'frozen',False):
-        return item
-    else:
-        return str(pathlib.Path(pathlib.Path(__file__).parent, item))
+    return str(pathlib.Path(pathlib.Path(__file__).parent, item))
