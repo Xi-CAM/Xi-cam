52c76f782b35857a90816d22531f5619d25cdea7
Ian Humphrey <ian.r.humphrey@gmail.com>
2019-08-15 09:23:56 -0700

    XPCS GUI support (ROI, tabview widget creation fail) (#14)
    * Add repr to BetterPolyLineROI
    
    * Handle tab view failing to create widgetcls

--- xicam/gui/widgets/ROI.py
+++ xicam/gui/widgets/ROI.py
@@ -5,10 +5,19 @@ from pyqtgraph.graphicsItems.ROI import Handle
 # MIXIN!~
 # Now with 100% more ROI!
 class BetterROI(ROI):
+    roi_count = 0
+    index = None
+
+    def __new__(cls, *args, **kwargs):
+        BetterROI.roi_count +=1
+        instance = ROI.__new__(cls, *args, **kwargs)
+        instance.index = cls.roi_count
+        return instance
+
     def __init__(self, *args, **kwargs):
         super(BetterROI, self).__init__(*args, **kwargs)
 
-        #
+
         for handledict in self.handles:  # type: dict
             handle = handledict['item']  # type: Handle
             handle.radius = handle.radius * 2
@@ -16,4 +25,6 @@ class BetterROI(ROI):
 
 
 class BetterPolyLineROI(BetterROI, PolyLineROI):
-    pass
+
+    def __repr__(self):
+        return f'ROI #{self.index}'
