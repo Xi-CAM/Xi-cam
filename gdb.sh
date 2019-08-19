267b632cb192d8d3ebce38e6a3b1d95f11cd8bc4
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-08-19 11:50:53 -0700

    Fix gdb.sh paths

--- gdb.sh
+++ gdb.sh
@@ -1,2 +1,2 @@
-source "/home/rp/.local/share/virtualenvs/xi-cam2-4wqyW8X3/bin/activate"
+source "/home/rp/PycharmProjects/xi-cam2/venv/bin/activate"
 PYTHONPATH="/home/rp/PycharmProjects/xi-cam2/" gdb -ex r --args python xicam/run_xicam.py
