9d31c3650bc70f69b15e3159671ccb649942387b
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-09-25 16:39:57 -0700

    Small fixes from Richard (#13)
    * Small fixes (#10)
    
    * Small fixes
    
    'min_threshold' is the second Input that should have been listet; changed name for consistency
    
    * prevent incorrect loading errors
    
    * Added sorting of processing plugins
    
    * fixed loading
    
    * Revert change to plugin discovery

--- xicam/plugins/__init__.py
+++ xicam/plugins/__init__.py
@@ -196,7 +196,7 @@ class XicamPluginManager(PluginManager):
         The default behavior is that each plugin is instanciated at load time; the class is thrown away.
         Add the isSingleton = False attribute to your plugin class to prevent this behavior!
         """
-        msg.logMessage(f"Instanciating {plugin_info.name} plugin object.")
+        msg.logMessage(f"Instanciating {plugin_info.name} plugin object.", level=msg.INFO)
 
         with load_timer() as elapsed:
             try:
@@ -369,8 +369,9 @@ class XicamPluginManager(PluginManager):
             if not success:
                 for element in (getattr(candidate_module, name) for name in dirlist):  # add filtering?
 
-                    self.load_element(element, candidate_infofile, plugin_info)
-
+                    success = self.load_element(element, candidate_infofile, plugin_info)
+                    if success:
+                        break
             if success:
                 msg.logMessage(f"{int(elapsed() * 1000)} ms elapsed while loading {plugin_info.name}", level=msg.INFO)
             else:
