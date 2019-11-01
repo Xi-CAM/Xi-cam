995b4f7c713990cc77ad1fba4b2db6917f03a202
Ian Humphrey <ian.r.humphrey@gmail.com>
2019-11-01 14:34:03 -0700

    Updated hint system

--- xicam/plugins/hints.py
+++ xicam/plugins/hints.py
@@ -2,39 +2,72 @@ from .processingplugin import Var, Input, ProcessingPlugin
 from typing import Dict, List
 import copy
 
+import numpy as np
+import pyqtgraph as pg
+
+from itertools import count
+
 
 class Hint(object):
+    canvas_cls = None
+
     def __init__(self, **kwargs):
         self.parent = None
         self.checked = False
         self.enabled = True
 
+    def init_canvas(self, **kwargs):
+        return self.canvas_cls()
+
     @property
     def name(self):
+        raise NotImplementedError
+
+    def remove(self):
         raise NotImplementedError
 
     def selective_copy(self, var_mapping: Dict):
         #TODO raise
         return self
 
+    def visualize(self, canvas):
+        raise NotImplementedError
+
 
 class PlotHint(Hint):
-    def __init__(self, x: Var, y: Var, **kwargs):
+    canvas_cls = pg.PlotWidget
+
+    def __init__(self, x: np.ndarray, y: np.ndarray, **kwargs):
+        super(PlotHint, self).__init__()
         if kwargs.get("name"):
             self._name = kwargs["name"]
-        super(PlotHint, self).__init__()
         self.x = x
         self.y = y
         self.kwargs = kwargs
+        self.item = None
+        self.canvas = None
 
     @property
     def name(self):
         if not self._name:
-            self._name = f"{self.y.name} vs. {self.x.name}"
+            left = self.kwargs.get('labels', dict()).get('left')
+            bottom = self.kwargs.get('labels', dict()).get('bottom')
+            if left and bottom:
+                self._name = f"{left} vs. {bottom}"
+            else:
+                self._name = "Plot"
         return self._name
 
-    def visualize(self, canvas, **canvases):
-        canvas.plot(self.x.value, self.y.value, **self.kwargs)
+    def visualize(self, canvas):
+        self.item = canvas.plot(self.x, self.y, **self.kwargs)
+        self.canvas = canvas
+
+    def remove(self):
+        self.canvas.removeItem(self.item)
+        self.item = None
+        if not list(filter(lambda item: isinstance(item, pg.PlotCurveItem), self.canvas.items())):
+            parent_widget = self.canvas.parent().parent()
+            parent_widget.removeTab(parent_widget.indexOf(self.canvas))
 
     def selective_copy(self, var_mapping: Dict):
         new_plot_hint = copy.copy(self)
@@ -44,73 +77,85 @@ class PlotHint(Hint):
 
 
 class VerticalROI(Hint):
+    # Design needs to be thought out here, could be either a plot widget or image view
+    # TODO -- range should not be a Var
+    # canvas_cls = tuple(pg.PlotWidget, pg.ImageView)
+
     def __init__(self, range: Var, **kwargs):
         super(VerticalROI, self).__init__()
+        self.ref_count.next()
         self.range = range
         self.kwargs = kwargs
+        self.canvas = None
 
     @property
     def name(self):
         return f"{self.parent.name} Vertical ROI"
 
-    def visualize(self, canvas, **canvases):  # TODO: callables?
+    def visualize(self, canvas):  # TODO: callables?
         from pyqtgraph import LinearRegionItem
 
-        canvas = canvases["imageview"]
-        if callable(canvas):
-            canvas = canvas()
-        canvas.addItem(LinearRegionItem(*self.range.value, **self.kwargs))
+        self.canvas = canvas
+        self.canvas.addItem(LinearRegionItem(*self.range.value, **self.kwargs))
 
     @property
     def name(self):
         return f"{self.parent.name} Vertical ROI"
+
 
 
 class ButtonHint(Hint):
+    # TODO -- activated should not be a Var
     targetattribute = "value"
 
     def __init__(self, activated: Var, iconpath: str):
-        super(ButtonHint, self).__init__()
-        self.activated = activated
-        self.iconpath = iconpath
-        self.enabled = False
-
-    def visualize(self, canvas, **canvases):
-        from qtpy.QtWidgets import QToolButton
-        from qtpy.QtGui import QIcon
-
-        canvas = canvases["toolbar"]  # type:QToolBar
-        if callable(canvas):
-            canvas = canvas()
-        button = QToolButton()
-        button.setIcon(QIcon(self.iconpath))
-        button.setCheckable(True)
-        button.toggled.connect(lambda state: setattr(self.activated, self.targetattribute, state))
-        canvas.addWidget(button)
-
-    @property
-    def name(self):
-        return f"{self.activated.name} button"
+        raise DeprecationWarning
+    #     super(ButtonHint, self).__init__()
+    #     self.activated = activated
+    #     self.iconpath = iconpath
+    #     self.enabled = False
+    #
+    # def visualize(self, canvas):
+    #     from qtpy.QtWidgets import QToolButton
+    #     from qtpy.QtGui import QIcon
+    #
+    #     canvas = canvases["toolbar"]  # type:QToolBar
+    #     if callable(canvas):
+    #         canvas = canvas()
+    #     button = QToolButton()
+    #     button.setIcon(QIcon(self.iconpath))
+    #     button.setCheckable(True)
+    #     button.toggled.connect(lambda state: setattr(self.activated, self.targetattribute, state))
+    #     canvas.addWidget(button)
+    #
+    # @property
+    # def name(self):
+    #     return f"{self.activated.name} button"
 
 
 class EnableHint(ButtonHint):
     targetattribute = "enabled"
 
     def __init__(self, parent: ProcessingPlugin, iconpath: str):
-        super(EnableHint, self).__init__(parent, iconpath)  # Ignore typing violation
-        self.iconpath = iconpath
-        self.enabled = False  # This 'enabled' means the hint's visibility can't be changed
-
-    @property
-    def name(self):
-        return f"{self.parent.name} toggle button"
+        raise DeprecationWarning
+    #     super(EnableHint, self).__init__(parent, iconpath)  # Ignore typing violation
+    #     self.iconpath = iconpath
+    #     self.enabled = False  # This 'enabled' means the hint's visibility can't be changed
+    #
+    # @property
+    # def name(self):
+    #     return f"{self.parent.name} toggle button"
 
 
 class ImageHint(Hint):
-    def __init__(self, image: Var, xlabel: str = None, ylabel: str = None, transform=None, z: int = None, **kwargs):
+    canvas_cls = pg.ImageView
+    ref_count = count(0)
+
+    def __init__(self, image, name=None, xlabel: str = None, ylabel: str = None, transform=None, z: int = None, **kwargs):
         if kwargs.get("name"):
             self._name = kwargs["name"]
         super(ImageHint, self).__init__()
+        next(self.ref_count)
         self.image = image
         self.xlabel = xlabel
         self.ylabel = ylabel
@@ -118,17 +163,25 @@ class ImageHint(Hint):
         self.z = z
         self.kwargs = kwargs
         self.enabled = False
+        self._name = name
+        self.canvas = None
+
+    def init_canvas(self):
+        return self.canvas_cls(view=pg.PlotItem(labels=dict(left=self.xlabel, bottom=self.ylabel)))
 
     @property
     def name(self):
         if not self._name:
-            self._name = f"Image of {self.image.name}"
+            self._name = "Image " + str(self.ref_count)
         return self._name
 
-    def visualize(self, canvas, **canvases):
-        canvas = canvases["imageview"]
-        if canvas:
-            canvas.setImage(self.image.value, **self.kwargs)
+    def visualize(self, canvas):
+        self.canvas = canvas
+        self.canvas.setImage(self.image, **self.kwargs)
+
+    def remove(self):
+        parent_widget = self.canvas.parent().parent()
+        parent_widget.removeTab(parent_widget.indexOf(self.canvas))
 
     def selective_copy(self, var_mapping: Dict):
         new_plot_hint = copy.copy(self)
@@ -137,12 +190,15 @@ class ImageHint(Hint):
 
 
 class CoPlotHint(Hint):
+    canvas_cls = pg.PlotWidget
+
     def __init__(self, *plothints: List[PlotHint], **kwargs):
         if kwargs.get("name"):
             self._name = kwargs["name"]
         super(CoPlotHint, self).__init__()
         self.plothints = plothints  # type: List[PlotHint]
         self.kwargs = kwargs
+        self.canvas = None
 
     @property
     def name(self):
@@ -150,6 +206,7 @@ class CoPlotHint(Hint):
             self._name = f"Plot of " + ", ".join([hint.name for hint in self.plothints])
         return self._name
 
-    def visualize(self, canvas, **canvases):
+    def visualize(self, canvas):
+        self.canvas = canvas
         for plothint in self.plothints:
-            canvas.plot(plothint.x, plothint.y, **{**plothint.kwargs, **self.kwargs})
+            self.canvas.plot(plothint.x, plothint.y, **{**plothint.kwargs, **self.kwargs})
