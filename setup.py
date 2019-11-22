372c3344ca5c543717a25d54d3dbdf447d9db93a
Dan Allan <dallan@bnl.gov>
2019-11-22 09:12:58 -0500

    Remove bluesky-browser dependency.

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["qtpy", "pathlib", "pyqtgraph", "qdarkstyle", "qtmodern", "qtconsole", "xicam.plugins", "xicam.core", "bluesky-browser"]
+    install_requires=["qtpy", "pathlib", "pyqtgraph", "qdarkstyle", "qtmodern", "qtconsole", "xicam.plugins", "xicam.core"]
     + pyqt,
     setup_requires=[],
     # List additional groups of dependencies here (e.g. development
