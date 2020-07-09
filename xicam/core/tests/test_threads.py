ba55c37d9aef56de28f0e90c2c39e3c3decf9085
ronpandolfi <ronpandolfi@gmail.com>
2020-07-09 11:47:17 -0700

    More thread tests

--- xicam/core/tests/test_threads.py
+++ xicam/core/tests/test_threads.py
@@ -49,3 +49,57 @@ def test_threads_iterator(qtbot):
     t.start()
     qtbot.waitSignal(t.sigFinished)
 
+
+def test_exit_before_thread(qtbot):
+    from xicam.core import threads
+    import time
+    from qtpy.QtWidgets import QMainWindow
+
+    window = QMainWindow()
+
+    def long_thread():
+        time.sleep(100000)
+
+    for i in range(1000):
+        t = threads.QThreadFuture(long_thread)
+
+        t.start()
+    time.sleep(.01)
+
+    window.deleteLater()
+
+def test_exit_before_decorated_thread(qtbot):
+    from xicam.core import threads
+    import time
+    from qtpy.QtWidgets import QMainWindow
+
+    window = QMainWindow()
+
+    @threads.method()
+    def long_thread():
+        time.sleep(100000)
+
+    for i in range(1000):
+        long_thread()
+
+    time.sleep(.01)
+
+    window.deleteLater()
+
+def test_qthreads_and_pythreads(qtbot):
+    from xicam.core import threads
+    import time
+    from qtpy.QtWidgets import QMainWindow
+
+    window = QMainWindow()
+
+    @threads.method()
+    def long_thread():
+        time.sleep(100000)
+
+    for i in range(1000):
+        long_thread()
+
+    time.sleep(.01)
+
+    window.deleteLater()
