99cef86d6ffafb58237b94f54c62d58d46682883
Ian Humphrey <ian.r.humphrey@gmail.com>
2020-07-21 16:19:07 -0700

    go back to using waitForWindowShown

--- tests/test_application.py
+++ tests/test_application.py
@@ -3,7 +3,7 @@ from pytestqt import qtbot
 from xicam.run_xicam import _main
 import pytest
 import sys
-from qtpy import API_NAME
+# from qtpy import API_NAME
 
 
 def test_application(qtbot):
@@ -11,8 +11,8 @@ def test_application(qtbot):
     main_window = _main(Namespace(nosplash=True), exec=False)
     qtbot.addWidget(main_window)
     print()  # this prevents Windows access violation err in test...
-    if API_NAME.lower() == "pyside2":
-        qtbot.wait(6000)  # give it time to finish loading
-        qtbot.waitForWindowShown(main_window)
-    else:
-        qtbot.waitExposed(main_window, 20000)
+    # if API_NAME.lower() == "pyside2":
+    qtbot.wait(6000)  # give it time to finish loading
+    qtbot.waitForWindowShown(main_window)
+    # else:
+    #     qtbot.waitExposed(main_window, 10000)
