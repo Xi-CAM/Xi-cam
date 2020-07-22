d7afa742b0bd65c8ae3586d8e35eecd950f3e58f
Ian Humphrey <ian.r.humphrey@gmail.com>
2020-07-21 21:45:42 -0700

    Adapt for recommended pyqt5 qtbot, add print to fix windows access violation (somehow)

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,11 +1,18 @@
+from argparse import Namespace
 from pytestqt import qtbot
 from xicam.run_xicam import _main
+import pytest
 import sys
+from qtpy import API_NAME
 
 
 def test_application(qtbot):
     sys.argv = sys.argv[:1]
-    main_window = _main([], exec=False)
+    main_window = _main(Namespace(nosplash=True), exec=False)
     qtbot.addWidget(main_window)
-    qtbot.wait(6000)  # give it time to finish loading
-    qtbot.waitForWindowShown(main_window)
+    print()  # this prevents Windows access violation err in test...
+    if API_NAME.lower() == "pyside2":
+        qtbot.wait(6000)  # give it time to finish loading
+        qtbot.waitForWindowShown(main_window)
+    else:
+        qtbot.waitExposed(main_window, 20000)
