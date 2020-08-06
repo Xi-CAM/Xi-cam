6d90d4a381cab2e655c0a2d3bed107bad370100c
ronpandolfi <ronpandolfi@gmail.com>
2020-08-06 14:11:31 -0700

    Refactor slicing propagation; fix linked slicing

--- xicam/gui/widgets/ndimageview.py
+++ xicam/gui/widgets/ndimageview.py
@@ -1,32 +1,61 @@
 from pyqtgraph import ImageView, InfiniteLine, mkPen, ScatterPlotItem, ImageItem, PlotItem, HistogramLUTWidget, \
     GraphicsView
 from qtpy.QtGui import QTransform, QPolygonF
-from qtpy.QtWidgets import QLabel, QErrorMessage, QSizePolicy, QPushButton, QHBoxLayout, QVBoxLayout, QComboBox, QWidget, QMenu, QAction, QGridLayout
+from qtpy.QtWidgets import QLabel, QErrorMessage, QSizePolicy, QPushButton, QHBoxLayout, QVBoxLayout, QComboBox, \
+    QWidget, QMenu, QAction, QGridLayout
 from qtpy.QtCore import Qt, Signal, Slot, QSize, QPointF, QRectF, QObjectCleanupHandler
 from xarray import DataArray
 import numpy as np
+from typing import Tuple, Dict
+from xicam.gui.widgets.ROI import BetterCrosshairROI
 
 
 class NDImageView(QWidget):
+    """
+    Top-level NDViewer widget
+
+    Internal structure is:
+
+    NDImageView
+    - SliceablePanel
+    |- SliceableGraphicsView
+    |- SliceablePanel
+     |- SliceableGraphicsView
+     |- SlieablePanel
+    ...
+    """
+
     def __init__(self):
         super(NDImageView, self).__init__()
         self.setLayout(QHBoxLayout())
-        self.setContentsMargins(0, 0, 0, 0)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.layout().setSpacing(0)
 
-        self.graphics_view = SliceablePanel()
+        self.graphics_view = SliceablePanel(parent=self)
 
         self.layout().addWidget(self.graphics_view)
         self.layout().addWidget(HistogramLUTWidget())
 
     def setData(self, data: DataArray):
-        self.graphics_view.setData(data)
+        self.graphics_view.setData(data, view_dims=data.dims[:2], slc={dim: 0 for dim in data.dims})
+
+    def updateSlicing(self, slice):
+        print('newslice:', slice)
+        for widget in self.findChildren(SliceablePanel):
+            widget.updateSlicing(slice)
 
 
 class SliceablePanel(QWidget):
-    def __init__(self):
-        super(SliceablePanel, self).__init__()
+    sigSlicingChanged = Signal(object)
+
+    def __init__(self, parent=None):
+        super(SliceablePanel, self).__init__(parent=parent)
+
+        assert parent
 
         self.setLayout(QGridLayout())  # TODO: use splitters
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.layout().setSpacing(0)
 
         self.full_view = SliceableGraphicsView()
         self.right_view = None
@@ -34,34 +63,57 @@ class SliceablePanel(QWidget):
         self.corner_view = None
         self.data = None
         self.slice = {}
+        self.view_dims = None
 
         self.layout().addWidget(self.full_view, 1, 0, 1, 1)
 
         self.full_view.sigToggleHorizontalSlice.connect(self.toggleHorizontalSlice)
         self.full_view.sigToggleVerticalSlice.connect(self.toggleVerticalSlice)
 
-    def setData(self, data:DataArray):
-        self.data = data
-        self.slice = {dim: 0 for dim in data.dims[2:]}
+        # Find top-level (NDImageView) widget
+        parent = self.parent()
+        while not isinstance(parent, NDImageView):
+            parent = parent.parent()
 
-        self.full_view.setData(data[self.slice])
-        if self.right_view:
-            self.right_view.setData(data)
-        if self.top_view:
-            self.top_view.setData(data)
-        if self.corner_view:
-            self.corner_view.setData(data)
+        self.sigSlicingChanged.connect(parent.updateSlicing)
+        self.full_view.crosshair.sigMoved.connect(self._sliceChangedByCrosshair)
+
+    def _sliceChangedByCrosshair(self):
+        view_dims = reversed(self.view_dims)
+        slc = {key: value for key, value in zip(view_dims, self.full_view.crosshair.pos())}
+        self.sigSlicingChanged.emit(slc)
+
+    @property
+    def sliced_data(self):
+        slc = {key: value for key, value in self.slice.items() if key not in self.view_dims}
+        data = self.data.sel(**slc, method='nearest').transpose(*self.view_dims)
+        return data
+
+    def updateSlicing(self, slc):
+        self.slice.update(slc)
+
+        self.full_view.setData(self.sliced_data)
+
+        new_crosshair_pos = self.slice[self.view_dims[1]], self.slice[self.view_dims[0]]
+        self.full_view.crosshair.setPos(new_crosshair_pos)
+
+    def setData(self, data: DataArray, view_dims: Tuple[str], slc: Dict):
+        self.data = data
+        self.view_dims = view_dims
+        self.slice = slc
+
+        self.full_view.setData(self.sliced_data)
 
     def toggleHorizontalSlice(self, enable):
         if enable:
             if not self.right_view:
-                self.right_view = SliceablePanel()
+                self.right_view = SliceablePanel(parent=self)
                 if self.data is not None:
                     # get the coordinate of the vertical slice
-                    slice = self.full_view.getCrosshairPos()[1]
+                    # slice = self.full_view.crosshair.pos()[1]
 
                     # set the right view to the same xarray, but with the second coordinate pre-sliced
-                    self.right_view.setData(self.data.sel(**{self.data.dims[1]: slice}, method='nearest'))
+                    self.right_view.setData(self.data, view_dims=self.getViewDims('right'), slc=self.slice)
             self.layout().addWidget(self.right_view, 1, 1, 1, 1)
         else:
             self.right_view.hide()
@@ -69,22 +121,23 @@ class SliceablePanel(QWidget):
     def toggleVerticalSlice(self, enable):
         if enable:
             if not self.top_view:
-                self.top_view = SliceablePanel()
+                self.top_view = SliceablePanel(parent=self)
                 if self.data is not None:
                     # get the coordinate of the horizontal slice
-                    slice = self.full_view.getCrosshairPos()[0]
-
-                    # pre-slice the first coord
-                    top_data: DataArray = self.data.sel(**{self.data.dims[0]: slice}, method='nearest')
-
-                    # transpose the next two dims (to match visually with the layout)
-                    top_data = top_data.transpose(top_data.dims[1], top_data.dims[0], ...)
+                    # slice = self.full_view.crosshair.pos()[0]
 
                     # set the top view to the same xarray, but with the first coordinate pre-sliced
-                    self.top_view.setData(top_data)
+                    self.top_view.setData(self.data, view_dims=self.getViewDims('top'), slc=self.slice)
             self.layout().addWidget(self.top_view, 0, 0, 1, 1)
         else:
             self.top_view.hide()
+
+    def getViewDims(self, quadrant: str):
+        depth_dim = self.data.dims[max(self.data.dims.index(self.view_dims[1]),self.data.dims.index(self.view_dims[0])) + 1]
+        if quadrant == 'right':
+            return self.view_dims[0], depth_dim
+        elif quadrant == 'top':
+            return depth_dim, self.view_dims[1]
 
 
 class SliceableGraphicsView(GraphicsView):
@@ -93,6 +146,8 @@ class SliceableGraphicsView(GraphicsView):
 
     def __init__(self):
         super(SliceableGraphicsView, self).__init__()
+
+        self.setContentsMargins(0, 0, 0, 0)
 
         # Add axes
         self.view = SliceableAxes()
@@ -107,11 +162,8 @@ class SliceableGraphicsView(GraphicsView):
         self.view.addItem(self.image_item)
 
         # add crosshair
-        linepen = mkPen("#FFA500")
-        self._vline = InfiniteLine((0, 0), angle=90, movable=True, pen=linepen)
-        self._hline = InfiniteLine((0, 0), angle=0, movable=True, pen=linepen)
-        self.view.addItem(self._vline)
-        self.view.addItem(self._hline)
+        self.crosshair = BetterCrosshairROI((0, 0), parent=self.view)
+        self.view.addItem(self.crosshair)
 
     def setData(self, data):
 
@@ -124,7 +176,7 @@ class SliceableGraphicsView(GraphicsView):
 
         # Position the image according to coords
         shape = data.shape
-        a = [(0, shape[-2]), (shape[-1]-1, shape[-2]), (shape[-1]-1, 1), (0, 1)]
+        a = [(0, shape[-2]), (shape[-1] - 1, shape[-2]), (shape[-1] - 1, 1), (0, 1)]
 
         b = [(ymin, xmin), (ymax, xmin), (ymax, xmax), (ymin, xmax)]
 
@@ -138,16 +190,13 @@ class SliceableGraphicsView(GraphicsView):
         QTransform.quadToQuad(quad1, quad2, transform)
 
         # Bind coords from the xarray to the timeline axis
-        #super(SliceableGraphicsView, self).setImage(img, autoRange, autoLevels, levels, axes, np.asarray(img.coords[img.dims[0]]), pos, scale, transform, autoHistogramRange, levelMode)
+        # super(SliceableGraphicsView, self).setImage(img, autoRange, autoLevels, levels, axes, np.asarray(img.coords[img.dims[0]]), pos, scale, transform, autoHistogramRange, levelMode)
         self.image_item.setImage(np.asarray(data))
         self.image_item.setTransform(transform)
 
         # Label the image axes
         self.view.setLabel('left', data.dims[-2])
         self.view.setLabel('bottom', data.dims[-1])
-
-    def getCrosshairPos(self):
-        return self._hline.pos()[1], self._vline.pos()[0]
 
     def updateImage(self, autoHistogramRange=True):
         ## Redraw image on screen
