a4f5fa812c6569a32d8e0e5eaab0e9874ab69b6c
ronpandolfi <ronpandolfi@gmail.com>
2020-08-19 14:27:49 -0700

    Refactor Workflow editor widget to use QListView and the checkstate/flags inherent to the model

--- xicam/gui/tests/test_linear_workflow.py
+++ xicam/gui/tests/test_linear_workflow.py
@@ -1,26 +1,12 @@
 from pytestqt import qtbot
-import pytest
-from xicam.plugins.operationplugin import (
-    display_name,
-    fixed,
-    input_names,
-    limits,
-    opts,
-    output_names,
-    output_shape,
-    plot_hint,
-    units,
-    visible,
-    ValidationError,
-    operation,
-)
 from xicam.core.execution import Workflow
 from xicam.gui.widgets.linearworkfloweditor import WorkflowEditor
 from xicam.core.tests.workflow_fixtures import square_op, sum_op, custom_parameter_op, custom_parameter_workflow, simple_workflow
 
 
-def test_simple(simple_workflow: Workflow, qtbot):
+def test_simple(simple_workflow: Workflow, square_op, qtbot):
     workflow_editor = WorkflowEditor(simple_workflow)
+    simple_workflow.add_operation(square_op.clone())
     workflow_editor.show()
     qtbot.addWidget(workflow_editor)
 
@@ -29,4 +15,4 @@ def test_custom_parameter(custom_parameter_workflow: Workflow, qtbot):
     workflow_editor = WorkflowEditor(custom_parameter_workflow)
     workflow_editor.show()
     qtbot.addWidget(workflow_editor)
-    workflow_editor.workflowview.selectRow(0)  # Note: models is empty here because pluginmanger hasn't finished load yet
+    workflow_editor.workflowview.setCurrentIndex(workflow_editor.workflowview.model().createIndex(0,0))  # Note: models is empty here because pluginmanger hasn't finished load yet
