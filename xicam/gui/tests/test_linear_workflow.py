09be52df55887381ed4d1a4a353e6246858d2805
ronpandolfi <ronpandolfi@gmail.com>
2020-08-28 09:54:15 -0700

    Add menu test

--- xicam/gui/tests/test_linear_workflow.py
+++ xicam/gui/tests/test_linear_workflow.py
@@ -16,6 +16,7 @@ from xicam.plugins.operationplugin import (
 )
 from xicam.core.execution import Workflow
 from xicam.gui.widgets.linearworkfloweditor import WorkflowEditor
+from xicam.plugins import manager
 from xicam.core.tests.workflow_fixtures import square_op, sum_op, custom_parameter_op, custom_parameter_workflow, simple_workflow
 
 
@@ -30,3 +31,15 @@ def test_custom_parameter(custom_parameter_workflow: Workflow, qtbot):
     workflow_editor.show()
     qtbot.addWidget(workflow_editor)
     workflow_editor.workflowview.selectRow(0)  # Note: models is empty here because pluginmanger hasn't finished load yet
+
+
+def test_menu(qtbot):
+    workflow_editor = WorkflowEditor(Workflow())
+    workflow_editor.show()
+    qtbot.addWidget(workflow_editor)
+
+    manager.qt_is_safe = True
+    manager.initialize_types()
+    manager.collect_plugins()
+
+    qtbot.wait(10000)
