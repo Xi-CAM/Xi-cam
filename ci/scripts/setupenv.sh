a47c8ff1eb05915aeb9a4b440730389d7b87961f
Ian Humphrey <ian.r.humphrey@gmail.com>
2020-12-17 15:04:39 -0800

    Customize fail-fast and turn on debug

--- ci/scripts/setupenv.sh
+++ ci/scripts/setupenv.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# -v verbose
+# -E allow ERR trap to fire
+# -e bash script exits immediately when a command fails
+# -u bash script treats unset variables as error and exits
+# -x debug (print commands)
+# -o exit code zero only if all commands in pipeline are 0
+set -vEeuxo pipefail
 
 repo_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." >/dev/null 2>&1 && pwd )"
 
