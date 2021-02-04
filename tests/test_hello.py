bf6a625697c01b28c77c7e8f8cc6c5073bef6498
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-02-04 15:49:35 -0800

    investigate if qtbot having issues on github actions for us

---
+++ tests/test_hello.py
@@ -0,0 +1,12 @@
+from qtpy import QtCore
+from pytestqt import qtbot
+
+
+def test_hello(qtbot):
+    widget = HelloWidget()
+    qtbot.addWidget(widget)
+
+    # click in the Greet button and make sure it updates the appropriate label
+    qtbot.mouseClick(widget.button_greet, QtCore.Qt.LeftButton)
+
+    assert widget.greet_label.text() == "Hello!"
