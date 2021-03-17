fe178541a468f633749c18d56ddf4fae520dba55
ronpandolfi <ronpandolfi@gmail.com>
2021-03-17 16:25:57 -0700

    Add numpy ingestor

--- xicam/core/__init__.py
+++ xicam/core/__init__.py
@@ -1,1 +1,2 @@
 from . import paths
+from . import formats
--- xicam/core/formats/NPYPlugin.py
+++ xicam/core/formats/NPYPlugin.py
@@ -1,5 +1,29 @@
+import mimetypes
+
+from bluesky_live.run_builder import RunBuilder
 from xicam.plugins.datahandlerplugin import DataHandlerPlugin, start_doc
 import numpy as np
+
+mimetypes.add_type('application/x-npy', '.npy')
+
+
+def ingest_npy(paths):
+    d = np.load(paths[0], allow_pickle=True)
+    data_keys = {'image': {'source': paths[0],
+                           'dtype': 'array',
+                           'shape': d.shape,
+                           'dims': ('dim_0', 'dim_1')}}
+
+    with RunBuilder() as builder:
+        builder.add_stream("primary",
+                           # NOTE: Put data in list, since Runbuilder.add_stream expects
+                           # a sequence number to add event_page
+                           data={'image': d},
+                           data_keys=data_keys
+                           )
+
+    builder.get_run()
+    yield from builder._cache
 
 
 class NPYPlugin(DataHandlerPlugin):
